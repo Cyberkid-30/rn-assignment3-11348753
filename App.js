b74420e8484b89e11331b8268fc734aa6501206e
Jan Phandoh <janphandoh@gmail.com>
2024-05-25 12:31:47 +0000

    changed background color

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.text2}>Kwabena.</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    // fontSize: 24,
+  },
+
+  text2: {
+    // fontWeight: "bold",
+  },
+});
